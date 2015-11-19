376f568e1a1b65deb0e869fb9c1c2a6912b91a0b
kocar <kocar@B276.mucam.mpg.de>
2015-11-19 09:29:02 +0100

    CreateMetaDataPage enhanced

--- src/spot/pages/CreateMetaDataPage.java
+++ src/spot/pages/CreateMetaDataPage.java
@@ -12,6 +12,9 @@ public class CreateMetaDataPage extends BasePage {
 	@FindBy (xpath=".//*[@id='mdProfileType']")
 	private List<WebElement> list;
 	
+	@FindBy (xpath=".//*[@id='profileForm:profile:0:j_idt248']")
+	private WebElement createMetaDataBlock;
+	
 	public CreateMetaDataPage(WebDriver driver) {
 		super(driver);
 		
@@ -20,6 +23,33 @@ public class CreateMetaDataPage extends BasePage {
 	
 	public void createMetaDataProfile() {
 		
+
+
+		createMetaDataBlocks(7);
+		
+		
+	}
+
+	private void createMetaDataBlocks(int number) {
+		
+		// 8 metadata fields are needed; one already exists, create seven more
+//		o	Datentyp Text
+//		o	Datentyp Person
+//		o	Datentyp Number
+//		o	Datentyp Date
+//		o	Datentyp Geolocation
+//		o	Datentyp License
+//		o	Datentyp Link
+//		o	Datentyp Publication
+		
+		for (int i=0; i<number; i++) {
+			createMetaDataBlock.click();
+			try{
+				Thread.sleep(2000);
+			} catch (Exception e) {
+				
+			}
+		}
 	}
 
 }
