0f9e1591837650c6ed341a9da64d4eaad423dbc9
helkv <helk@mpdl.mpg.de>
2018-09-05 10:47:27 +0200

    #34 Add waitForReloadOfElement method to SeleniumWrapper
    - Add the waitForReloadOfElement method, which should be used to wait
    for a partial reload of a page (if an element is reloaded because of an
    ajax request).

--- src/test/base/SeleniumWrapper.java
+++ src/test/base/SeleniumWrapper.java
@@ -26,6 +26,19 @@ public class SeleniumWrapper {
 	}
 	
 	/**
+	 * Wait until a certain component/element of the page is reloaded. <br>
+	 * Uses waiting for the element to become stale (not present on the DOM) to detect that the old element doesn't exist anymore. <br>
+	 * The elementToBecomeReloaded must be the WebElement that will be reloaded. It must be initialized before the event, that led to the partial reload. <br>
+	 * The elementToBecomeReloaded must NOT be a Page Object field, which uses lazy loading.
+	 * 
+	 * @param wait The WebDriverWait, which defines the wait timeout in seconds
+	 * @param elementToBecomeReloaded An element of the current page which will be reload 
+	 */
+	public static void waitForReloadOfElement(WebDriverWait wait, WebElement elementToBecomeReloaded) {
+		wait.until(ExpectedConditions.stalenessOf(elementToBecomeReloaded));
+	}
+	
+	/**
 	 * Wait until the new/next page is loaded. <br>
 	 * Uses waiting for an old element to become invisible (not present on the DOM) to detect that the old page doesn't exist anymore. <br>
 	 * The oldElement must be a WebElement of the old page. It must be initialized before the event, that led to the page reload. <br>
