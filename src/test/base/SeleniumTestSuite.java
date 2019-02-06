3395244b8030368d095fa293b5bf79af7d44bb3f
helkv <helk@mpdl.mpg.de>
2019-02-06 18:15:33 +0100

    Temporarily run tests on qa-caesar
    - To test architectural changes (which are deployed on qa-caesar
    ) test will Temporarily run on qa-caesar

--- src/test/base/SeleniumTestSuite.java
+++ src/test/base/SeleniumTestSuite.java
@@ -23,25 +23,29 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
 public class SeleniumTestSuite {
-	
+
 	private static WebDriver driver;
-	
-	/** properties file with required login information for test user and admin **/
-	private static Properties properties;	
+
+	/**
+	 * properties file with required login information for test user and admin
+	 **/
+	private static Properties properties;
 	public static final String PROPERTIES_FILE_NAME = "testData.properties";
-	
+
 	public static final boolean HEADLESS = true;
-	
+
 	public static final String QA_EDMOND = "http://qa-edmond.mpdl.mpg.de/imeji/";
 	public static final String QA_IMEJI = "http://qa-imeji.mpdl.mpg.de/imeji/";
-	
+	private static final String QA_CESAR = "http://qa-caesar.mpdl.mpg.de/imeji/";
+
 	public static final String DEV_IMEJI = "https://dev-imeji.mpdl.mpg.de/imeji/";
-	
-	public static final String TEST_ENV_URL = QA_IMEJI;
-//	public static final String TEST_ENV_URL = DEV_IMEJI;
+
+	// public static final String TEST_ENV_URL = QA_IMEJI;
+	// public static final String TEST_ENV_URL = DEV_IMEJI;
+	public static final String TEST_ENV_URL = QA_CESAR;
 
 	private static final Logger log4j = LogManager.getLogger(SeleniumTestSuite.class.getName());
-	
+
 	@Parameters("browserType")
 	@BeforeSuite
 	public void launchDriver(String browserType) throws MalformedURLException, FileNotFoundException {
@@ -50,7 +54,7 @@ public class SeleniumTestSuite {
 
 		setDriver(browserType);
 		loadPropertiesFile();
-		
+
 		driver.navigate().to(TEST_ENV_URL);
 		log4j.info("Test environment url loaded.");
 		log4j.info("Running Tests on: " + TEST_ENV_URL);
@@ -61,87 +65,100 @@ public class SeleniumTestSuite {
 		properties = new Properties();
 		InputStream input = this.getClass().getClassLoader().getResourceAsStream(PROPERTIES_FILE_NAME);
 
-		try {	
+		try {
 			properties.load(input);
 			log4j.info("Successfully loaded testData.properties");
-		} 
-		catch (IOException e) {
+		} catch (IOException e) {
 			log4j.error("Properties file with login data couldn't be loaded");
 			e.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
-		finally {
-        	if(input != null) {
-        		try {
-        			input.close();
-        		}
-        		catch (IOException e) {
-        			e.printStackTrace();
-        		}
-        	}
-        }
 	}
 
 	/**
-	 * Dependent on the specified browserType, the corresponding browser will be launched.
+	 * Dependent on the specified browserType, the corresponding browser will be
+	 * launched.
+	 * 
 	 * @param browserType
 	 */
 	private void setDriver(String browserType) {
 		switch (browserType) {
-			case "chrome":
-				driver = initChromeDriver();
-				break;
-			case "firefox":
-				driver = initFirefoxDriver();
-				break;
-			default:
-				log4j.warn("Browser : " + browserType 
-						+ " is invalid. Launching default browser instead (Firefox)...");
-				driver = initFirefoxDriver();
+		case "chrome":
+			driver = initChromeDriver();
+			break;
+		case "firefox":
+			driver = initFirefoxDriver();
+			break;
+		default:
+			log4j.warn("Browser : " + browserType + " is invalid. Launching default browser instead (Firefox)...");
+			driver = initFirefoxDriver();
 		}
 		driver.manage().window().maximize();
 		log4j.info("Window maximised.");
-		
+
 		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-		// TODO: Remove the implicitlyWait. Check and fix all tests that  wont't run if there is no implicitlyWait.
-		// The implicitlyWait is a problem wherever driver.findElements()-method finds no element, then it waits the whole implicitlyWait time..
+		// TODO: Remove the implicitlyWait. Check and fix all tests that wont't
+		// run if there is no implicitlyWait.
+		// The implicitlyWait is a problem wherever driver.findElements()-method
+		// finds no element, then it waits the whole implicitlyWait time..
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
 	private WebDriver initFirefoxDriver() {
 		log4j.info("Launching Firefox browser...");
-		// The system property webdriver.gecko.driver must be set to the webdriver-executable-file -> this is done by Maven!
+		// The system property webdriver.gecko.driver must be set to the
+		// webdriver-executable-file -> this is done by Maven!
 		log4j.info("Found system property webdriver.gecko.driver: " + System.getProperty("webdriver.gecko.driver"));
 		FirefoxOptions options = new FirefoxOptions();
 		options.setCapability("marionette", true);
-		
+
 		FirefoxBinary binary = new FirefoxBinary();
 		options.setBinary(binary);
 		options.setHeadless(HEADLESS);
 		FirefoxProfile profile = initFirefoxProfile();
 		options.setProfile(profile);
-		
+
 		return new FirefoxDriver(options);
 	}
-	
+
 	private WebDriver initChromeDriver() {
 		log4j.info("Launching Chrome browser...");
-		// The system property webdriver.chrome.driver must be set to the webdriver-executable-file -> this is done by Maven!
+		// The system property webdriver.chrome.driver must be set to the
+		// webdriver-executable-file -> this is done by Maven!
 		log4j.info("Found system property webdriver.chrome.driver: " + System.getProperty("webdriver.chrome.driver"));
-		ChromeOptions options = new ChromeOptions();		
+
+		// ChromeDriverService service =
+		// ChromeDriverService.createDefaultService();
+
+		ChromeOptions options = new ChromeOptions();
 		options.setCapability("marionette", true);
 		options.setHeadless(HEADLESS);
-		options.addArguments("--window-size=1920,1200");
+		// options.addArguments("--proxy-server='direct://'");
+		// options.addArguments("--proxy-bypass-list=*");
+		// options.addArguments("--no-sandbox");
+		// options.addArguments("--window-size=1920,1200");
+		// options.addArguments("--headless", "window-size=1024,768",
+		// "--no-sandbox");
+		// options.addArguments("headless", "no-sandbox");
 		return new ChromeDriver(options);
 	}
-	
+
 	private FirefoxProfile initFirefoxProfile() {
 		FirefoxProfile profile = new FirefoxProfile();
-		profile.setPreference("browser.download.folderList",2);
+		profile.setPreference("browser.download.folderList", 2);
 		profile.setPreference("browser.download.manager.showWhenStarting", false);
-		profile.setPreference("browser.download.dir","./target/downloads");
-		profile.setPreference("browser.helperApps.neverAsk.saveToDisk","application/msword, application/csv, application/ris, text/csv, image/png, application/pdf, text/html, text/plain, application/zip, application/x-zip, application/x-zip-compressed, application/download, application/octet-stream");
+		profile.setPreference("browser.download.dir", "./target/downloads");
+		profile.setPreference("browser.helperApps.neverAsk.saveToDisk",
+				"application/msword, application/csv, application/ris, text/csv, image/png, application/pdf, text/html, text/plain, application/zip, application/x-zip, application/x-zip-compressed, application/download, application/octet-stream");
 		profile.setPreference("browser.download.manager.alertOnEXEOpen", false);
-		profile.setPreference("browser.download.manager.focusWhenStarting", false);  
+		profile.setPreference("browser.download.manager.focusWhenStarting", false);
 		profile.setPreference("browser.download.useDownloadDir", true);
 		profile.setPreference("browser.helperApps.alwaysAsk.force", false);
 		profile.setPreference("browser.download.manager.closeWhenDone", true);
@@ -149,15 +166,15 @@ public class SeleniumTestSuite {
 		profile.setPreference("browser.download.manager.useWindow", false);
 		profile.setPreference("services.sync.prefs.sync.browser.download.manager.showWhenStarting", false);
 		profile.setPreference("pdfjs.disabled", true);
-		
+
 		return profile;
 	}
-	
+
 	@AfterSuite
 	public static void quitDriver() {
 		log4j.info("Quitting driver...");
 		driver.quit();
-	}	
+	}
 
 	public static WebDriver getDriver() {
 		return driver;
@@ -165,7 +182,7 @@ public class SeleniumTestSuite {
 
 	public static Properties getProperties() {
 		return properties;
-	}	
+	}
 
 	@BeforeTest
 	public void testBeforeTest() {
